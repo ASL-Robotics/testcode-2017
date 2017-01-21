76bb9e6f23400271a44cd72a6fd40cb2f7c279bf
Will V <will_vauclain@asl.org>
2017-01-21 10:26:58 +0000

    SendableChooser test code created

---
+++ src/org/usfirst/frc/team1797/robot/commands/DualfireCommand.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team1797.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team1797.robot.Robot;
+import org.usfirst.frc.team1797.robot.RobotMap;
+
+/**
+ *
+ */
+public class DualfireCommand extends Command {
+	public DualfireCommand() {
+		// Use requires() here to declare subsystem dependencies
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		System.out.println("Dual fire ran!");
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		RobotMap.zero.set(RobotMap.joystick.getRawAxis(1));
+		RobotMap.one.set(RobotMap.joystick.getRawAxis(1));
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
+}
---
+++ src/org/usfirst/frc/team1797/robot/commands/FullSteamAheadCommand.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team1797.robot.commands;
+
+import org.usfirst.frc.team1797.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class FullSteamAheadCommand extends Command {
+
+    public FullSteamAheadCommand() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	System.out.println("Full steam ahead!");
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	RobotMap.zero.set(RobotMap.joystick.getRawAxis(1));
+    	RobotMap.one.set(RobotMap.joystick.getRawAxis(1));
+    	RobotMap.two.set(RobotMap.joystick.getRawAxis(1));
+    	RobotMap.three.set(RobotMap.joystick.getRawAxis(1));
+    	RobotMap.four.set(RobotMap.joystick.getRawAxis(1));
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
